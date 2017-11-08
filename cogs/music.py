df68e181953ef2d4a75bfa70808741803864d6dc
Ary Y <gtownx36@gmail.com>
2017-11-08 20:09:22 +0530

    some comments

--- cogs/music.py
+++ cogs/music.py
@@ -6,7 +6,8 @@ import discord
 from discord.ext import commands
 from utils import playlist
 from utils import player
-from utils import ytsearch
+from utils.ytsearch import ytsearch
+
 
 class Music:
 
@@ -30,15 +31,20 @@ class Music:
         if '-s' in message.content.split():
             searchmode = 1
 
+        # Handle funky embed connecting
         if message.guild not in bot.vc_clients:
             np_embed = discord.Embed(title='Connecting...', colour=0xffffff)
             np_embed.set_thumbnail(url='https://i.imgur.com/DQrQwZH.png')
             trying_msg = await message.channel.send(embed=np_embed)
+
+            # Error if you're trying to listen music out of thin air/not in a voice channel
             if message.author.voice is None:
                 np_embed = discord.Embed(title='Error', description='You are not in a voice channel', colour=0xffffff)
                 np_embed.set_thumbnail(url='https://imgur.com/B9YlwWt.png')
                 await trying_msg.edit(embed=np_embed)
                 return
+
+            # This timeout never triggers, not sure
             try:
                 vc = await message.author.voice.channel.connect(timeout=6.0, reconnect=True)
             except asyncio.TimeoutError:
@@ -52,22 +58,31 @@ class Music:
                                      description='summoned by **%s**' % message.author.mention, colour=0xffffff)
             np_embed.set_thumbnail(url='https://imgur.com/F95gtPV.png')
             await trying_msg.edit(embed=np_embed)
+
+        # Smart way to go through the message and find out what's a flag and where our song name starts from
         for n, item in enumerate(message.content.split()[1:], 1):
             if not item.startswith('-'):
                 break
+
         song_name = ' '.join(message.content.split()[n:])
+
+        # If we already have a player for this server, use it
         if message.guild in bot.players:
             vc = bot.vc_clients[message.guild]
             mplayer = bot.players[message.guild]
-            print('\nplay waiting for download lock\n')
+        # Make a player object if we don't
         else:
             vc = bot.vc_clients[message.guild]
             pl = playlist.Playlist(bot)
             print(dir(vc.ws))
             mplayer = player.Player(bot, vc, pl)
             bot.players[message.guild] = mplayer
+
+        # Using the player's qlock, ensuring that always the first received request is queued
         with await mplayer.qlock, message.channel.typing():
-            print('\nplay got download lock\n')
+            print('\nplay got Q lock\n')
+
+            # If it's a link
             if 'watch?' in song_name:
                 info = await bot.downloader.extract_info(bot.loop, song_name, download=False, process=False,
                                                          retry_on_error=True)
@@ -75,13 +90,18 @@ class Music:
                                                        info['title'], info['duration'], effect, info['thumbnail'])
                 await message.channel.send("**%s** was added to the queue at position %s, %s" % (
                     entry['title'], position, mplayer.playlist.estimate_time(position, mplayer)))
+
+            # If it's a playlist
             elif 'list' in song_name:
                 info = await bot.downloader.extract_info(bot.loop, song_name, download=False, process=False,
                                                          retry_on_error=True)
                 preparing_msg = await message.channel.send("Processing playlist...")
                 position = await mplayer.playlist.async_pl(info['webpage_url'], message.author, message.channel)
                 await preparing_msg.edit("Your playlist was added!")
+
+            # Plain text
             else:
+                # If search flag was provided, use ytsearch
                 if not searchmode:
                     info = await bot.downloader.extract_info(bot.loop, 'ytsearch1:'+song_name, download=False,
                                                              process=True, retry_on_error=True)
@@ -89,6 +109,7 @@ class Music:
                                                            message.channel, info['entries'][0]['title'],
                                                            info['entries'][0]['duration'], effect,
                                                            info['entries'][0]['thumbnails'][0]['url'], song_name)
+                # If not, we're passing it up to extract_info
                 else:
                     song = await ytsearch(bot, message, song_name)
                     info = await bot.downloader.extract_info(bot.loop, song[1], download=False, process=False,
@@ -98,6 +119,8 @@ class Music:
                                                            song_name)
                 await message.channel.send("**%s** was added to the queue at position %s, %s" % (
                     entry['title'], position, mplayer.playlist.estimate_time(position, mplayer)))
+
+            # Prepare the entry, music player, its time
             bot.loop.create_task(mplayer.prepare_entry(position - 1))
 
 
